ce7cfa4e58c6e129a3bdd65483e90bafab189225
Glen112 <escobar.glen@gmail.com>
2023-12-30 16:17:04 -0300

    actualizandoElementos

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@ const form = document.getElementById('novoItem')
 const lista = document.querySelector('.lista')
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
-function creaElemento(item){
-  const newLi = document.createElement('li')
-  newLi.classList.add('item');
-
-  const cuantosItem = document.createElement('strong')
-  cuantosItem.innerHTML = item.quantity;
-  newLi.appendChild(cuantosItem);
-  newLi.innerHTML += item.name;
-  lista.appendChild(newLi);
-
- }
 items.forEach((e) => {
   creaElemento(e)
 })
@@ -23,14 +12,24 @@ form.addEventListener('submit', (e) => {
   const name = e.target.elements['nome'];
   const quantity = e.target.elements['quantidade'];
 
+  const existe = items.find((e) => e.name === name.value);
+  // console.log(existe)
+
   const itemActual = {
    'name': name.value,
    'quantity': quantity.value
   }
 
-  creaElemento(itemActual)
+  if(existe){
+    itemActual.id = existe.id
+    // console.log(existe)
+    actualizaElemento(itemActual)
+  }else{
+    itemActual.id = items.length;
+    creaElemento(itemActual)
+    items.push(itemActual)
 
-  items.push(itemActual)
+  }
 
   localStorage.setItem('items', JSON.stringify(items));
 
@@ -38,7 +37,22 @@ form.addEventListener('submit', (e) => {
   quantity.value = "";  
 
 })
+function creaElemento(item){
+  const newLi = document.createElement('li')
+  newLi.classList.add('item');
+
+  const cuantosItem = document.createElement('strong')
+  cuantosItem.innerHTML = item.quantity;
+  cuantosItem.dataset.id = item.id;
+  newLi.appendChild(cuantosItem);
+  newLi.innerHTML += item.name;
+  lista.appendChild(newLi);
+
+ }
 
 
+function actualizaElemento(items){
+  document.querySelector("[data-id='"+items.id+"']").innerHTML = items.quantity
 
+}
 
